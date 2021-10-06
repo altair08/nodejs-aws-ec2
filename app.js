631c33e7a5c40acfe23174748bad044b31c282ec
thinesh <thinesh@gmail.com>
2021-10-06 13:09:28 +0530

    simple api

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>pipeline_status: Success</h4>');
+})
+
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
+})
+
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
