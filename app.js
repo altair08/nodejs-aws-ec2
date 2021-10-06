f34150315dc6b9567d60a0dc24fe76073288ed3c
thinesh <thinesh@gmail.com>
2021-10-06 13:37:52 +0530

    update

--- app.js
+++ app.js
@@ -3,21 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Node.js Express Demo App</h1> <h4>pipeline_status: Success again</h4>');
+  res.send('<h1>Node.js Express Demo App</h1> <h4>pipeline_status: Successfully updated</h4>');
 })
 
-app.get('/products', (req, res) => {
-  res.send([
-    {
-      productId: '101',
-      price: 100
-    },
-    {
-      productId: '102',
-      price: 150
-    }
-  ])
-})
+
 
 app.listen(port, ()=> {
   console.log(`Demo app is up and listening to port: ${port}`);
